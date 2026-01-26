29a5864536e6561d7ee62e57a2c5620ae98592f7
maryh <maryhug.viviana@gmail.com>
2026-01-26 17:23:38 -0500

    vista login.js creado con estilos

---
+++ src/views/login.js
@@ -0,0 +1,45 @@
+// Función que retorna el template HTML del login
+export function loginTemplate() {
+    return `
+         <main class="auth-container">
+    <section class="auth-card">
+
+      <header class="auth-header">
+        <h1>Observatorio Urbano y Ambiental</h1>
+        <p>Acceso a plataforma GovTech</p>
+      </header>
+
+      <form class="auth-form">
+        <div class="form-group">
+          <label for="email">Correo electrónico</label>
+          <input id="email" type="email" placeholder="admin@govtech.com" required />
+        </div>
+
+        <div class="form-group">
+          <label for="password">Contraseña</label>
+          <input id="password" type="password" placeholder="••••••••" required />
+        </div>
+
+        <button type="submit" class="btn btn-primary">
+          Iniciar sesión
+        </button>
+
+        <p class="auth-error hidden">
+          Credenciales inválidas. Intenta nuevamente.
+        </p>
+      </form>
+
+      <footer class="auth-footer">
+        <p>Proyecto educativo – Riwi GovTech</p>
+      </footer>
+
+    </section>
+  </main>
+    `;
+}
+
+
+// Función que maneja la lógica del componente
+export function loginScript() {
+
+}
