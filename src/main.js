db00e290ff20e4ea4017310bd5c58d82d0023d8e
EmmanuelArangoV <emmanuelarangov@gmail.com>
2026-01-27 11:51:37 -0500

    Draft of router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { renderDetailView } from './views/detail.js';
+import {Navbar } from "./components/Navbar.js";
+import {renderFooter} from "./components/Footer.js";
+import {router} from "./router/router.js";
+
 
 const app = document.getElementById('app');
-app.appendChild(renderDetailView())
+
+export function render(viewNode) {
+    app.innerHTML = '';
+
+    app.appendChild(Navbar());
+    app.appendChild(viewNode);
+    app.appendChild(renderFooter());
+}
+
+window.addEventListener('hashchange', router);
+window.addEventListener('load', router);
